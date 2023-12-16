384febc8b6416e36ed2cdc384dc5df8b18807b19
Tomky312 <106925620+Tomky312@users.noreply.github.com>
2023-12-16 22:17:54 +0100

    Add files via upload

--- 01.py
+++ 01.py
@@ -1,3 +1,19 @@
 from __future__ import annotations
 
-# BLANK
+lines = []
+line = "1mhggggg3"
+lines.append(line)
+
+def get_digits(line):
+    numbers = []
+    for symbol in line:
+        if symbol.isnumeric():
+            numbers.append(symbol)
+
+    return numbers
+
+num = get_digits(line)
+print(num)
+
+
+
